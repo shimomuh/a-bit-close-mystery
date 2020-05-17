343eee2523468b8f963814688c75e14ee6cf9e3e
shimomuh <shimomuh0501@gmail.com>
2020-05-18 02:10:32 +0900

    refactor hierarchy

---
+++ src/components/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Index.scss';
+
+const Index: React.FC = () => {
+  return (
+    <section className="index">
+      <h2>謎解き一覧</h2>
+      <p>Comming Soon...</p>
+    </section>
+  );
+}
+
+export default Index;
